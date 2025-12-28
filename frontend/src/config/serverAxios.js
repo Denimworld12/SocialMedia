0e44e34fc906c77f4b514c2b5365a21f3cdb3deb
Nikhil Gupta <nikhilrakeshg@gmail.com>
2025-12-29 00:52:10 +0530

    added path to the backend to NEXT_PUBLIC_BACKEND_URL

--- frontend/src/config/serverAxios.js
+++ frontend/src/config/serverAxios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const serverAxios = axios.create({
-    baseURL: process.env.BACKEND_URL || "http://localhost:9080",
+    baseURL: process.env.NEXT_PUBLIC_BACKEND_URL || "http://localhost:9080",
     timeout: 10000,
 });
 
